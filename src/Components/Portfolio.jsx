fe5e79a0b7b146750e8acd2b942b5958b71bfb87
Dante Schrantz <149951163+DanteSc03@users.noreply.github.com>
2024-02-05 17:22:01 +0100

    Update Portfolio.jsx

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Cadeira AI",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Created a chatbot using Rasa Forms to integrate into phone lines and make reservations for restaurants.",
+    url: "https://cadeiraai.github.io/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Time Spent Analysis (R)",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Created a statistical and Data Analysis of how people spend their time.",
+    url: "https://dantesc03.github.io/projects.html",
   },
   {
-    title: "My Resume Site",
+    title: "BrewStop",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Created a tea steeping device to facilitate tea steeping on the go.",
+    url: "https://dantesc03.github.io/projects.html",
   },
 ];
 
